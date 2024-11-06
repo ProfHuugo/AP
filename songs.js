2da0d41214c4dc76e056ae482f6c8ae84c72cec0
ProfHuugo <168227984+ProfHuugo@users.noreply.github.com>
2024-11-06 18:37:50 -0300

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        src: "Apaguei_pra_todos.mp3",
+        name: "Ferrugem - Apaguei pra todos",
+    },
+    {
+        src: "Deixa_Acontecer.mp3",
+        name: "Grupo Revelação - Deixa Acontecer",
+    },
+    {
+        src: "Deixa_Alagar.mp3",
+        name: "Grupo Revelação - Deixa Alagar ",
+    },
+    {
+        src: "Ela.mp3",
+        name: "Ferrugem e Sorriso Maroto - Ela",
+    },
+    {
+        src: "Temporal.mp3",
+        name: "Thiaginho - Temporal/Velocidade da Luz",
+    },
+];
